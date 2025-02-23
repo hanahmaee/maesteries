491ad5ab40aa568f4c7d9db6ebe1321ecb6831c7
hanahmaee <hanahmaeespineda@gmail.com>
2025-02-23 15:53:40 +0800

    Initial commit with new app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <div className='container bg-amber-50 mx-auto'>
+        <h1 className='text-8xl text-black'>tailwind</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
