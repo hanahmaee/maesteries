4945ce08478e4af2b60f0065f77ac09585a8b4f9
hanahmaee <hanahmaeespineda@gmail.com>
2025-04-29 19:54:18 +0800

    Library Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/NavBar/Navbar'
 import About from './components/About/About'
 import Footer from './components/Footer/Footer'
+import Library from './components/Library/Library' // <-- create this page if not yet done
 
-const App = () => { 
+const App = () => {
   return (
-    <div className='overflow-x-hidden'>
-      <Navbar/>
-      <About/>
-      <Footer/>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<About />} /> {/* Replace with your homepage */}
+        <Route path="/library" element={<Library />} />
+        <Route path="/about" element={<About />} />
+        {/* Add more pages if needed */}
+      </Routes>
+      <Footer />
+    </Router>
   )
 }
 
